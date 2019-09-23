e59a58e5a8f9e22aa40ead2ebe4f716c08c41a6e
bawarian <6309024+bawarian@users.noreply.github.com>
2019-09-23 12:20:49 +0700

    Add files via upload

---
+++ storage/framework/views/092553d52804988b4ef74f6712850973b23f1f19.php
@@ -0,0 +1,15 @@
+<div class="flex-center position-ref full-height">
+	<div class="content">
+		<div class="title m-b-md">
+			<a href="<?php echo e(url('room/part1')); ?>">Part 1</a>
+		</div>
+
+		<div class="title m-b-md">
+			<a href="<?php echo e(url('room/part2')); ?>">Part 2</a>
+		</div>
+	</div>
+</div>
+
+
+<?php echo $__env->make('layouts.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php echo $__env->make('layouts.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel\resources\views/main.blade.php ENDPATH**/ ?>
